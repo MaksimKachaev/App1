0a2998f1a20d83f28485b12c1661e850b5a35f5d
MaksimKachaev1996@gmail.com <MaksimKachaev1996@gmail.com>
2023-11-27 22:26:16 +0300

    Update

--- gui.py
+++ gui.py
@@ -4,18 +4,32 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+listBox = sg.Listbox(values=functions.get_todos(), key='todos',
+                     enable_events=True, size=[45, 10])
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]],
+edit_button = sg.Button("Edit")
+
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button], [listBox, edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo =  values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
             functions.write_todos(todos)
         case sg.WIN_CLOSED:
             break
