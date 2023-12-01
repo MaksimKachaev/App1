dc9951e61302aa26b37aa7a2c2d3912ab2328973
Maksim Kachaev <MaksimKachaev1996@gmail.com>
2023-12-01 13:18:49 +0300

    Ready hw17

---
+++ homework/converters.py
@@ -0,0 +1,3 @@
+def convert(feet, inch):
+    meters = float(feet) * 0.3048 + float(inch) * 0.0254
+    return meters
--- homework/hw17.py
+++ homework/hw17.py
@@ -1,36 +1,27 @@
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
+from converters import convert
 
-sg.theme("DarkAmber")
+feet_label = Sg.Text("Enter feet:")
+feet_input = Sg.Input(key="feets")
 
-feet_label = sg.Text()
-feet_input = sg.Input(key="feet")
-inch_label = sg.Text()
-inch_input = sg.Input(key="inch")
-convert_button = sg.Button("Convert")
+inch_label = Sg.Text("Enter inches")
+inch_input = Sg.Input(key="inches")
 
+convert_button = Sg.Button("Convert")
+update_output = Sg.Text(key="output")
 
 layout = [[feet_label, feet_input],
           [inch_label, inch_input],
-          [convert_button]]
+          [convert_button, update_output]]
 
-window = sg.Window("Convertor", layout)
+window = Sg.Window("Convertor", layout)
 
 while True:
     event, values = window.read()
-    feets = values["feet"]
-    inches = values["inch"]
-    match event:
-        case "Convert":
-            convert_to_meters = float(feets) * 0.3048 + float(inches) * 0.0254
-            #+ float(inches) * 0,245
-            print(convert_to_meters, 'm')
-            window['']
-            # feet_to_inches = feet_input * 0,3048 layout.i #+ #layout.inch * 0,0254
-            # print(feet_to_inches)
-        case sg.WIN_CLOSED:
-            break
-
+    feet = float(values["feets"])
+    inch = float(values["inches"])
+    convertor = convert(feet, inch)
+    window['output'].update(value=f"{convertor} m", text_color="white")
+    if Sg.WIN_CLOSED:
+        break
 window.close()
-
-#Inches = Feet * 12
-#d(m) = d(ft) × 0,3048 + d(in) × 0,0254
